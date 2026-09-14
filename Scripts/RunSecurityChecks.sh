46fd278b026668c577b690d43471f5640e9ebcc7
RileyMarquis3 <15660294+RileyMarquis3@users.noreply.github.com>
2026-09-13 23:50:15 -0500

    Add configuration and sanity checks to script

--- Scripts/RunSecurityChecks.sh
+++ Scripts/RunSecurityChecks.sh
@@ -1,13 +1,44 @@
 #!/bin/env bash
 
+###############################################################################################################################################
+# CONFIGURATION #                                                                                                                            #
+###############################################################################################################################################
+
+# Define Config Dir #
+CONF_DIR="conf"
+
+# Define Functions Dir #
+FUNC_DIR="functions"
+
+# Define Our Colors Library #
+COLORS_FILE="Colors.conf"
+
 # Define Our Functions Library #
 FUNCTIONS_FILE="RunSecurityChecks.bfunc"
 
-# Sanity Check: Functions File #
-if [ ! -f "$FUNCTIONS__FILE" ]; then
-    echo "ERROR: Functions Library $FUNCTIONS_FILE not found...quitting!"
-    exit 1
+###############################################################################################################################################
+# Sanity Checks #                                                                                                                             #
+###############################################################################################################################################
+
+# Sanity Check: Colors File Exists?
+if [ -f "$CONF_DIR"/"$COLORS_FILE" ]; then
+  echo "Sourcing Colors File..."
+  source "$CONF_DIR"/"$COLORS_FILE"
+else
+  echo "ERROR: Colors Library $CONF_DIR/$COLORS_FILE not found...quitting!"
+  return 1
 fi
+
+# Sanity Check: Functions File #
+if [ -f "$FUNC_DIR"/"$FUNCTIONS_FILE" ]; then
+  echo "Sourcing Functions File..."
+  source "$FUNC_DIR"/"$FUNCTIONS_FILE"
+else
+  echo "ERROR: Functions Library $FUNC_DIR/$FUNCTIONS_FILE not found...quitting!"
+  return 1
+fi
+
+############################################################################################################################################
 
 # Main Program #
 
